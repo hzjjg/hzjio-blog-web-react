f8eab5886001bc2017b2fe3c062c00a3124ea44a
hzj <hzj3@meitu.com>
2018-11-26 16:43:01 +0800

    fix

---
+++ src/containers/articleList/articleList.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+class ArticleList extends React.Component {
+    public render() {
+        return (
+            <div className="ArticleList">
+                header
+            </div>
+        )
+    }
+}
+
+export default ArticleList;
