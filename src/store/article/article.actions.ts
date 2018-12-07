d2568889ed68f1426e2f7a9867024215b1239037
hzj <hzj3@meitu.com>
2018-12-07 11:36:28 +0800

    update

--- src/store/article/article.actions.ts
+++ src/store/article/article.actions.ts
@@ -1,20 +1,30 @@
-import { createAsyncAction } from "typesafe-actions";
+import { createAsyncAction, action } from "typesafe-actions";
 import { ArticleEntity } from "src/models/article.entity";
 import { ArticlePageQuery } from "src/apis/article.api";
 
 enum Actions {
-    fetchList = 'ARTICLE_FETCH_LIST',
-    fetchListSucceed = 'ARTICLE_FETCH_LIST_SUCCEED',
-    fetchListFailed = 'ARTICLE_FETCH_LIST_FAILED',
 
     fetchdOne = 'ARTICLE_FETCH_ONE',
     fetchOneSucceed = 'ARTICLE_FETCH_ONE_SUCCEED',
     fetchOneFailed = 'ARTICLE_FETCH_ONE_FAILED',
 
+    fetchList = 'ARTICLE_FETCH_LIST',
+    fetchListSucceed = 'ARTICLE_FETCH_LIST_SUCCEED',
+    fetchListFailed = 'ARTICLE_FETCH_LIST_FAILED',
+
+    create = '',
+    createSucceed = '',
+    createFailed = '',
+
     update = '',
     updateSucceed = '',
-    updateFailed = ''
+    updateFailed = '',
+
+    remove = '',
+    removeSucceed = '',
+    removeFailed = '',
 }
+
 
 /** 获取文章列表 */
 export const fetchList = createAsyncAction(
@@ -30,4 +40,23 @@ export const fetchOne = createAsyncAction(
     Actions.fetchOneFailed,
 )<{ id: string }, ArticleEntity, Error>();
 
-// export const update = createAsyncAction()
+/** 增加文章 */
+export const create = createAsyncAction(
+    Actions.create,
+    Actions.createSucceed,
+    Actions.createFailed
+)<ArticleEntity, { id: string }, Error>();
+
+/** 修改文章 */
+export const update = createAsyncAction(
+    Actions.update,
+    Actions.updateSucceed,
+    Actions.updateFailed
+)<ArticleEntity, { id: string }, Error>();
+
+/** 删除文章 */
+export const remove = createAsyncAction(
+    Actions.remove,
+    Actions.removeSucceed,
+    Actions.removeFailed
+)<{ id: string }, { id: string }, Error>();
